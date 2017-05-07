3ba3748f34ce1b9341cc3d0e41dad230964be3fc
Mahendra Kalkura <mahendra@kalkura.com>
2017-05-07 15:17:07 +0530

    main.go

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"sort"
 
+	"github.com/dustin/go-humanize"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -17,9 +21,46 @@ import (
 const filesClientSecret = "client_secret.json"
 const filesToken = "token.json"
 
+type bySize []*drive.File
+
+func (items bySize) Len() int {
+	return len(items)
+}
+
+func (items bySize) Swap(one int, two int) {
+	items[one], items[two] = items[two], items[one]
+}
+
+func (items bySize) Less(one int, two int) bool {
+	return items[one].QuotaBytesUsed > items[two].QuotaBytesUsed
+}
+
 func getClient(background context.Context, config *oauth2.Config) *http.Client {
 	token := getToken(config)
 	return config.Client(background, token)
+}
+
+func getConfig() *oauth2.Config {
+	bytes, bytesErr := ioutil.ReadFile(filesClientSecret)
+	if bytesErr != nil {
+		log.Fatalf("%v\n", bytesErr)
+	}
+
+	config, configErr := google.ConfigFromJSON(bytes, drive.DriveMetadataReadonlyScope)
+	if configErr != nil {
+		log.Fatalf("%v\n", configErr)
+	}
+
+	return config
+}
+
+func getService(client *http.Client) *drive.Service {
+	service, serviceErr := drive.New(client)
+	if serviceErr != nil {
+		log.Fatalf("%v\n", serviceErr)
+	}
+
+	return service
 }
 
 func getToken(config *oauth2.Config) *oauth2.Token {
@@ -74,35 +115,23 @@ func setToken(token *oauth2.Token) {
 	json.NewEncoder(file).Encode(token)
 }
 
-func main() {
-	background := context.Background()
-	bytes, bytesErr := ioutil.ReadFile(filesClientSecret)
-	if bytesErr != nil {
-		panic(bytesErr)
-	}
-
-	config, configErr := google.ConfigFromJSON(bytes, drive.DriveMetadataReadonlyScope)
-	if configErr != nil {
-		panic(configErr)
-	}
-
-	client := getClient(background, config)
-
-	server, serverErr := drive.New(client)
-	if serverErr != nil {
-		panic(serverErr)
-	}
-
+func fetch(pageSize int64) []*drive.File {
 	files := []*drive.File{}
-
+	background := context.Background()
+	config := getConfig()
+	client := getClient(background, config)
+	service := getService(client)
 	pageToken := ""
 	for {
-		query := server.Files.
+		query := service.
+			Files.
 			List().
-			Fields("nextPageToken, files(id, quotaBytesUsed, name)").
-			OrderBy("name").
-			PageSize(1000).
-			Q("parents in 'root'")
+			Fields("nextPageToken, files(id, name, quotaBytesUsed, webContentLink, webViewLink)").
+			IncludeTeamDriveItems(true).
+			OrderBy("quotaBytesUsed desc").
+			PageSize(pageSize).
+			Spaces("drive").
+			SupportsTeamDrives(true)
 		if pageToken != "" {
 			query = query.PageToken(pageToken)
 		}
@@ -116,11 +145,50 @@ func main() {
 			break
 		}
 	}
+	return files
+}
 
-	len := len(files)
-	if len > 0 {
-		for _, file := range files {
-			fmt.Println(fmt.Sprintf("%72s: [%9d] %s", file.Id, file.QuotaBytesUsed, file.Name))
-		}
+func report(files []*drive.File, limit int) {
+	log.Println("")
+
+	totalFilesInt := len(files)
+	totalFilesInt64 := int64(totalFilesInt)
+	totalFilesHumanize := humanize.Comma(totalFilesInt64)
+	log.Printf("Total Files: %v\n", totalFilesHumanize)
+
+	totalBytesInt := 0
+	totalBytesInt64 := int64(totalBytesInt)
+	for _, file := range files {
+		totalBytesInt64 = totalBytesInt64 + file.QuotaBytesUsed
 	}
+	totalBytesUint64 := uint64(totalBytesInt64)
+	totalBytesHumanize := humanize.Bytes(totalBytesUint64)
+	log.Printf("Total Size : %v\n", totalBytesHumanize)
+
+	sort.Sort(bySize(files))
+
+	if limit > totalFilesInt {
+		limit = totalFilesInt
+	}
+	files = files[0:limit]
+
+	log.Println("")
+
+	for _, file := range files {
+		bytesUint64 := uint64(file.QuotaBytesUsed)
+		bytesHumanize := humanize.Bytes(bytesUint64)
+		link := file.WebViewLink
+		if link == "" {
+			link = file.WebContentLink
+		}
+		log.Printf("%s [%9s] %s\n", link, bytesHumanize, file.Name)
+	}
+}
+
+func main() {
+	pageSize := flag.Int64("m", 999, "Page size")
+	limit := flag.Int("l", 10, "Limit")
+	flag.Parse()
+	files := fetch(*pageSize)
+	report(files, *limit)
 }
